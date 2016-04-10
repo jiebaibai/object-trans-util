22225b926f3469be47b44f83ff671e75fbfd6535
wangdao <623155403@qq.com>
2016-04-11 00:33:47 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # object-util
+transformNestedToSimple ：把 {a:{b:{c:222, d:222}, f: 3333}} 嵌套对象转换为：
+{"a.b.c" : 222, ”a.b.d“ : 222，”a.f“: 3333}；
+
+
+transformSimpleToNested: 把 {"a.b.c" : 222, ”a.b.d“ : 222，”a.f“: 3333} 转换为 {a:{b:{c:222, d:222}, f: 3333}}
---
+++ src/index.js
@@ -0,0 +1,65 @@
+import assign from 'assign-deep';
+
+function transformToSimple(object, prefix, newObject) {
+  if (object) {
+    for (const fieldName in object) {
+      if (object.hasOwnProperty(fieldName)) {
+        const fieldValue = object[fieldName];
+
+        if (fieldValue && fieldValue.constructor === Object) {
+          const newPrefix = (prefix ? (prefix + fieldName) : fieldName) + '.'; 
+          transformToSimple(fieldValue, newPrefix, newObject);
+        } else {
+          const newFieldName = (prefix || '') + fieldName;
+          newObject[newFieldName] = fieldValue; // eslint-disable-line no-param-reassign
+        }
+      }
+    }
+  } else {
+    newObject = null; // eslint-disable-line no-param-reassign
+  }
+}
+
+export function transformNestedToSimple(object) {
+  const newObject = {};
+
+  transformToSimple(object, null, newObject);
+
+  return newObject;
+}
+
+
+function generateObj(names, value, newObject) {
+  const name = names[names.length - 1];
+
+  names.splice(names.length - 1, 1);
+  if (names.length > 0) {
+    let tmp = {};
+
+    if (newObject.hasOwnProperty(name)) {
+      tmp = newObject[name];
+    }
+    tmp[name] = value;
+    generateObj(names, tmp, newObject);
+  } else {
+    newObject[name] = value; // eslint-disable-line no-param-reassign
+  }
+}
+
+export function transformSimpleToNested(object) {
+  let newObj = {};
+
+  for (const fieldName in object) {
+    if (object.hasOwnProperty(fieldName)) {
+      const fieldNames = fieldName.split('.');
+      const fieldValue = object[fieldName];
+
+      const tmp = {};
+      generateObj(fieldNames, fieldValue, tmp);
+
+      newObj = assign({}, newObj, tmp);
+    }
+  }
+
+  return newObj;
+}
